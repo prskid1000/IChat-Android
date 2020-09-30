f40ed70d13f708000e1f15fba62fbc4f65db2db1
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2020-09-30 23:31:30 +0530

    Update

--- app/src/main/java/com/prskid1000/ichat/MainActivity.java
+++ app/src/main/java/com/prskid1000/ichat/MainActivity.java
@@ -1,8 +1,12 @@
 package com.prskid1000.ichat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_NETWORK_STATE) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.ACCESS_NETWORK_STATE},102);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] {Manifest.permission.INTERNET},103);
+        }
+
         getSupportActionBar().hide();
         Button button = (findViewById(R.id.button2));
         button.setOnClickListener(new View.OnClickListener() {
